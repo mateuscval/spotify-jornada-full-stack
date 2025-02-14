c59e0d2586bd6b9a6a70759ec8d2bea3df460acc
Mateus Amaral <mateuscval@gmail.com>
2025-02-14 16:34:28 -0300

    Component Main created

--- back-end/src/App.jsx
+++ back-end/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
 };
 
 export default App;
